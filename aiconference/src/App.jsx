04befe08954f09fd90c09c2cc42bdc70415545b4
YOUR USER NAME <narenvaithiyaa@gmail.com>
2025-04-08 00:07:25 +0530

    reg and about added

--- aiconference/src/App.jsx
+++ aiconference/src/App.jsx
@@ -4,15 +4,20 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Comitte from './comitte'
 import ThemeSection from './Theme/theme.jsx'
+import Reg from './reg-about/reg.jsx'
+import About from './reg-about/about.jsx'
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div style={{ color: '#FFFFFF' }}>
+      <About />
       <ThemeSection />
       <Comitte />
+      <Reg />
     </div>
   );
 }
+
 
 export default App;
